4fa275f6236f4068ecbc0eadc19ba8dcae7ea52c
ziko07 <zikoku07@gmail.com>
2019-09-05 19:06:34 +0600

    last

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -3,18 +3,12 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-test',
   // template: '<div>Hi this is template</div>',
-  template: `<h2>Welcome {{name}}</h2>
-  <h2 class="text-success">My Smart Code</h2>
-  <h2 [class]="danger">Test My Smart Code</h2>
-  <h2 [class.text-danger]="hasError">Another Test My Smart Code</h2>
-  <!--<h2>{{2+2}}</h2>-->
-  <!--<h2>{{'hello' + 'world'}}</h2>-->
-  <!--<h2>{{ name.length }}</h2>-->
-  <!--<h2>{{ name.toUpperCase() }}</h2>-->
-  <!--<h2>{{ greetUser() }}</h2>-->
-  <!--<h2>{{ siteUrl }}</h2>-->
-    <input [id]="myId" type="text" value="Habib">
-    <input [disabled]="isDisabled" [id]="myId" type="text" value="Habib">
+  template: `
+    <h2>
+      Welcome {{name}}
+    </h2>
+    <button (click)="onClick($event)">Greet</button>
+    {{greeting}}
   `,
   // styleUrls: ['./test.component.scss']
   styles: [`
@@ -41,6 +35,12 @@ export class TestComponent implements OnInit {
   public danger = 'text-danger'
   public hasError = false;
   public isSpecial = true;
+  public highlightColor = 'blue';
+  public greeting = "";
+  public titleStyles = {
+    color: "blue",
+    fontStyle: "italic"
+  }
   public messageClasses = {
    "text-success": !this.hasError,
    "text-danger": this.hasError,
@@ -57,6 +57,11 @@ export class TestComponent implements OnInit {
 
   greetUser() {
     return 'Hello ' + this.name;
+  }
+
+  onClick(event) {
+    this.greeting = event.type;
+    console.log(event);
   }
 
 }
